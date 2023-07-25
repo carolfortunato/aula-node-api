a39b85ec70fc58c6b160ad909ee8c9f6dfa7cab3
carol fortunato <carolfortunato@outlook.com>
2023-07-25 20:25:13 -0300

    mudança no index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const message = { message: 'API ok'}
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'API ok'})
+  res.status(200).json(message)
 })
 
 app.listen(port, () => {
